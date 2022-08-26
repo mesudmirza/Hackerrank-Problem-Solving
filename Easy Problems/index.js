ade83c835c3f28c130c4a9ca69b4a54ad7758fd7
mesud <mesudmirzezade717@gmail.com>
2022-08-26 16:26:36 +0400

    diagonal difference

--- Easy Problems/index.js
+++ Easy Problems/index.js
@@ -41,3 +41,18 @@ function aVeryBigSum(arr) {
     
     return sum;
 }
+
+
+// 5.Diagonal Difference
+function diagonalDifference(arr) {
+    let leftDiagonal = 0;
+    let rightDiagonal = 0;
+    
+    for (let i=0; i<arr.length; i++) {
+        leftDiagonal += arr[i][i];
+        rightDiagonal += arr[i][arr.length - i - 1];
+    }
+    
+    return Math.abs(leftDiagonal - rightDiagonal);
+}
+
