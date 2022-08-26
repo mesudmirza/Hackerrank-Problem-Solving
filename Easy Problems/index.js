7fffaf0809d39f7533c535e7d04c6547032276e3
mesud <mesudmirzezade717@gmail.com>
2022-08-26 16:23:09 +0400

    compare the triplets

--- Easy Problems/index.js
+++ Easy Problems/index.js
@@ -13,6 +13,19 @@ function simpleArraySum(arr) {
     for (let i=0; i<arr.length; i++) {
         sum += arr[i];
     }
-    
+
     return sum;
+}
+
+
+// 3.Compare the Triplets
+function compareTriplets(a, b) {
+    let points = [0, 0];
+    
+    for (let i=0; i<a.length; i++) {
+        if (a[i] > b[i]) points[0]++;
+        if (a[i] < b[i]) points[1]++;
+    }
+    
+    return points;
 }
