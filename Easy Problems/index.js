cbdefa50b52016ba76307bcd8246ac96ca9bff87
mesud <mesudmirzezade717@gmail.com>
2022-08-26 16:27:52 +0400

    plus minus

--- Easy Problems/index.js
+++ Easy Problems/index.js
@@ -56,3 +56,26 @@ function diagonalDifference(arr) {
     return Math.abs(leftDiagonal - rightDiagonal);
 }
 
+
+// 6.Plus Minus
+function plusMinus(arr) {
+    let len = arr.length;
+    let positive = 0;
+    let negative = 0;
+    let zero = 0;
+    
+    for (let i=0; i<len; i++) {
+        if (arr[i]>0) {
+            positive += 1;
+        } else if (arr[i]<0) {
+            negative += 1;
+        } else {
+            zero += 1;
+        }
+    }
+    
+    console.log((positive/len).toFixed(6));
+    console.log((negative/len).toFixed(6));
+    console.log((zero/len).toFixed(6));
+}
+
