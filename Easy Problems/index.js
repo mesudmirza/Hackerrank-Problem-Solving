abaa1d162ace260572a6b56fa5c95398fe81b151
mesud <mesudmirzezade717@gmail.com>
2022-08-26 16:20:54 +0400

    simple array sum

--- Easy Problems/index.js
+++ Easy Problems/index.js
@@ -4,3 +4,15 @@
 function solveMeFirst(a, b) {
     return a + b; 
 }
+
+
+// 2.Simple Array Sum
+function simpleArraySum(arr) {
+    let sum = 0;
+
+    for (let i=0; i<arr.length; i++) {
+        sum += arr[i];
+    }
+    
+    return sum;
+}
