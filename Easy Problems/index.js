95a12475f15f8ce3fa10ad75522548cbc3193fbe
mesud <mesudmirzezade717@gmail.com>
2022-09-04 18:17:20 +0400

    bill division

--- Easy Problems/index.js
+++ Easy Problems/index.js
@@ -234,3 +234,21 @@ function divisibleSumPairs(n, k, ar) {
 
     return output.length;
 }
+
+
+// 17.Bill Division
+function bonAppetit(bill, k, b) {
+    let sum = 0;
+    
+    for (let i = 0; i < bill.length; i++) {
+        sum += bill[i];
+    }
+    
+    let total = (sum - bill[k]) / 2;
+    
+    if (b <= total) {
+        console.log('Bon Appetit');
+    } else {
+        console.log(b - total);
+    }
+}
